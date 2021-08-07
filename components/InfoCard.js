66968481cfb1bdeb6ab2abda8572c8f7b6fdf4ae
Roman <grevzi@gmail.com>
2021-08-07 11:06:16 +0300

    Search page  - added bar-of-progress for page load  - added calendar  - added search result  - small fixes

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Image from "next/image";
-import { HeartIcon } from "@heroicons/react/outline";
+import { HeartIcon, StarIcon } from "@heroicons/react/outline";
 
 const InfoCard = ({img, location, title, description, star, price, total}) => {
     return (
-        <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition-all duration-200 ease-out">
+        <div
+            className="flex items-center cursor-pointer py-7 border-b first:border-t hover:opacity hover:scale-105 transition-all duration-200 ease-out">
             <div className="relative w-40 h-24 md:w-80 md:h-52 flex-shrink-0">
                 <Image
-                    className="rounded-lg"
+                    className="rounded-lg lg:rounded-2xl"
                     src={img}
                     layout="fill"
                     objectFit="cover"
@@ -19,7 +20,21 @@ const InfoCard = ({img, location, title, description, star, price, total}) => {
                     <p>{location}</p>
                     <HeartIcon className="w-7 h-7 cursor-pointer"/>
                 </div>
-                <h3 className="text-gray-500">{description}</h3>
+                <h4 className="text-xl">{title}</h4>
+                <div className="border-b w-10 pt-2"/>
+                <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
+
+                <div className="flex justify-between items-end pt-5">
+                    <p className="flex items-center">
+                        <StarIcon className="h-5 text-red-400"/>
+                        {star}
+                    </p>
+
+                    <div>
+                        <p className="text-lg font-semibold pb-2 lg:text-2xl">{price}</p>
+                        <p className="text-right font-extralight">{total}</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- pages/search.js
+++ pages/search.js
@@ -9,11 +9,13 @@ import InfoCard from "../components/InfoCard";
 
 const Search = ({searchResults}) => {
     const router = useRouter()
-    const {location, guests, startDate, endDate} = router.query
+    const {location = '', guests = 1, startDate = '', endDate= ''} = router.query
 
-    const formattedStartDate = format(new Date(startDate), 'dd MMMM yy')
-    const formattedEndDate = format(new Date(endDate), 'dd MMMM yy')
+    const formattedStartDate = startDate ? format(new Date(startDate), 'dd MMMM yy') : ''
+    const formattedEndDate = endDate ? format(new Date(endDate), 'dd MMMM yy') : ''
     const range = `${formattedStartDate} - ${formattedEndDate}`
+
+    const placeholder = location ? `${location} | ${range} | ${guests} guest ${guests > 1 && 's'}` : null
 
     return (
         <div>
@@ -22,38 +24,44 @@ const Search = ({searchResults}) => {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <Header placeholder={`${location} | ${range} | ${guests} guest ${guests > 1 && 's'}`}/>
+            <Header placeholder={placeholder}/>
 
             <main className="max-w-7xl mx-auto px-8 sm:px-16">
 
                 <div className="flex">
 
-                    <section className="flex-grow pt-14">
-                        <p className="text-xs">300+ Stays - {range} - for {guests} number of
-                            guest{guests > 1 && 's'}</p>
-                        <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
+                    {placeholder ? (
+                        <section className="flex-grow pt-14">
+                            <p className="text-xs">300+ Stays - {range} - for {guests} number of
+                                guest{guests > 1 && 's'}</p>
+                            <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
 
-                        <div className="hidden lg:inline-flex space-x-3 text-gray-800 mb-5">
-                            <button className="button" type="button">Cancellation flexibilities</button>
-                            <button className="button" type="button">Type of place</button>
-                            <button className="button" type="button">Price</button>
-                            <button className="button" type="button">Rooms of beds</button>
-                            <button className="button" type="button">More filters</button>
-                        </div>
+                            <div className="hidden lg:inline-flex space-x-3 text-gray-800 mb-5">
+                                <button className="button" type="button">Cancellation flexibilities</button>
+                                <button className="button" type="button">Type of place</button>
+                                <button className="button" type="button">Price</button>
+                                <button className="button" type="button">Rooms of beds</button>
+                                <button className="button" type="button">More filters</button>
+                            </div>
 
-                        <div className="flex flex-col">
-                            {searchResults?.map((item, i) => <InfoCard
-                                key={i}
-                                img={item.img}
-                                location={item.location}
-                                title={item.title}
-                                price={item.price}
-                                star={item.star}
-                                description={item.description}
-                                total={item.total}
-                            />)}
+                            <div className="flex flex-col">
+                                {searchResults?.map((item, i) => <InfoCard
+                                    key={i}
+                                    img={item.img}
+                                    location={item.location}
+                                    title={item.title}
+                                    price={item.price}
+                                    star={item.star}
+                                    description={item.description}
+                                    total={item.total}
+                                />)}
+                            </div>
+                        </section>
+                    ) : (
+                        <div className="flex-grow pt-14">
+                            <p className="text-gray-600">Start your search</p>
                         </div>
-                    </section>
+                    )}
 
                 </div>
 
